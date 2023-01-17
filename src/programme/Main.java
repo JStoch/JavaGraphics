8c9fab49394d167c38da989b62010ee395a2453f
J.Stoch <j.stochniel@gmail.com>
2023-01-17 20:06:34 +0100

    Added tests

--- src/programme/Main.java
+++ src/programme/Main.java
@@ -3,17 +3,15 @@ package programme;
 import programme.items.ComplexItem;
 import programme.items.TextItem;
 import programme.items.primitive.Segment;
-import programme.items.primitive.shapes.Circle;
+import programme.items.primitive.shapes.*;
 import programme.items.primitive.shapes.Rectangle;
-import programme.items.primitive.shapes.Star;
-import programme.items.primitive.shapes.Triangle;
 
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         Scene scene = new Scene();
-        ComplexItem c = new ComplexItem();
+        /*ComplexItem c = new ComplexItem();
         c.addChild(new Star(new Point(80, 80), 70, 7, 5, false));
         c.addChild(new Rectangle(new Point(100, 15), 40, 60, true));
         c.addChild(new Circle(new Point(130, 130), 20, true));
@@ -23,6 +21,11 @@ public class Main {
         scene.addItem(c);
         //c.translate(new Point(200, 200));
         scene.addItem(new TextItem(new Point(200, 180), new Font("Arial", Font.BOLD, 20), "tu przenieś"));
+
+        */
+        scene.addItem(SingletonTriangle.getInstance(new Point(50, 40), new Point(80, 45), new Point(50, 60), true));
+        scene.addItem(SingletonTriangle.getInstance(new Point(150, 140), new Point(180, 145), new Point(150, 160), true));
+
         scene.draw();
     }
 }
